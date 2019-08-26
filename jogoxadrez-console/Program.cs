e83ac95d43b1313fe8066c32c1b08b923105d30e
Felipe Lima <f.sousa230@gmail.com>
2019-08-26 16:41:55 -0300

    Criando a classe Posicao

--- jogoxadrez-console/Program.cs
+++ jogoxadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace jogoxadrez_console
 {
@@ -6,7 +7,7 @@ namespace jogoxadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p = new Posicao();
         }
     }
 }
