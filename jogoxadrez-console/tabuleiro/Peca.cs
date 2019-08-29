3c557dcfca837dd7abac8f9cd23917ba817dda58
Felipe Lima <f.sousa230@gmail.com>
2019-08-29 09:43:30 -0300

    Fazendo os primeiros testes de movimentacao

--- jogoxadrez-console/tabuleiro/Peca.cs
+++ jogoxadrez-console/tabuleiro/Peca.cs
@@ -19,6 +19,9 @@ namespace tabuleiro.Tabuleiro
             Tab = tab;
         }
 
-        
+        public void IncrementarQteMovimentos()
+        {
+            QtdeMovimentos++;
+        }
     }
 }
